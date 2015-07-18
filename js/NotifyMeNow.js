1068056512eff3104b7519711688596148eda534
Kirtan <kirtanthakkar4u@gmail.com>
2015-07-18 18:28:26 +0530

    Timeout to notification to 10 sec

--- js/NotifyMeNow.js
+++ js/NotifyMeNow.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 			//Initialization of the div to show the notifications, default false
 			var initialization = false;
 			var idCounter = 0;
+			var notificationTimeout = 10000;   //10 Seconds (Default Notification hiding time)
 			
 			$.fn.NotifyMeNow = function(param){
 				
@@ -18,17 +19,11 @@ $(document).ready(function(){
 						'left':'45%'   //Initial Testing
 					});
 					
-					//Initialize Function
+					//Initialize Functions
 					
-					//On Close hiding the notification
+					//On Close hiding the notification by delay 0 sec
 					$(this).on("click",".notifyMeClose",function(){
-						var notification = $(this).parent(".notifyMeNotification-container");
-						notification.addClass("exitAnimation");
-						setTimeout(function(){
-							notification.addClass("lol").slideUp(100,function(){
-								notification.remove()
-							});
-						},100);
+						hideNotification($(this).parent(".notifyMeNotification-container"),0);
 					});
 					
 					// Set initialization to true after initialization is complete
@@ -37,13 +32,33 @@ $(document).ready(function(){
 				
 				//Create New Notification
 				var newNotification = '<div class="notifyMeNotification-container" id="notifyMeNowNotification'+(++idCounter)+'">'+param+'<span class="notifyMeClose">&times;</div>';
+				
+				// Add the notification to notification container
 				$(this).append(newNotification);
+				
+				//Add a entry Animation class to show the notification
 				$("#notifyMeNowNotification"+idCounter).addClass("entryAnimation");
 				
+				//Autohide notification call
+				hideNotification($("#notifyMeNowNotification"+idCounter),notificationTimeout);
 				
 			};
 			
-			
+			function hideNotification(notificationDiv,hideDelayTime)
+			{
+				// Calling setTimeout to hide notification after called delay time
+				setTimeout(function(){
+					// Calling exit animation class to hide the notification
+					notificationDiv.addClass("exitAnimation");
+					
+					// After hiding the notification, make that hidden div close with slideUp function to move notification below that to move up.
+					setTimeout(function(){
+						notificationDiv.slideUp(100,function(){
+							notificationDiv.remove()
+						});
+					},100);
+				},hideDelayTime);
+			}
 			
 			
 			
